f07778294569825f4f685e915ebfaa2c51456f8d
FabianJohnThl <107192633+FabianJohnThl@users.noreply.github.com>
2025-03-12 16:43:20 +0100

    fix topic

--- launch/col_pth_launch.py
+++ launch/col_pth_launch.py
@@ -7,7 +7,7 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='col_pth', executable='col_pth',
-            remappings=[('path_in', '/path'),                         # Map your input path to your 
+            remappings=[('path_in', '/lio_livox/odometry_path_mapped'),                         # Map your input path to your 
                         ('path_colored', '/colored_path')],           # Map the merged and filtered cloud output to the desired output topic
             parameters=[{'colorizer': 'RM520N',                       # string to select a source for colorizing: {'RM520N', 'DUMMY'}
                          'movement_min_distance': 1.0                 # Minimum distance to be moved on path before drawing/querying new actual value for colorizing
